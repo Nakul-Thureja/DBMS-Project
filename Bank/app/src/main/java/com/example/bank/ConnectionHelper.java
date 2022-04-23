e9a5e7494badc7271b0ea68f739fb4ef732c0fa7
akshat-3 <82972138+akshat-3@users.noreply.github.com>
2022-04-23 18:43:47 +0530

    Android App Commit #5

---
+++ Bank/app/src/main/java/com/example/bank/ConnectionHelper.java
@@ -0,0 +1,36 @@
+package com.example.bank;
+
+import android.annotation.SuppressLint;
+import android.os.StrictMode;
+import android.util.Log;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConnectionHelper {
+    Connection con;
+    String uname, pass, ip, port, database;
+    @SuppressLint("NewApi")
+    public Connection connectionclass(String uname, String pass){
+        ip = "192.168.56.125";
+        database = "BankingV2";
+        uname = "Akshat";
+        pass = "Saini";
+        port = "1433";
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        Connection connection = null;
+        String ConnectionURL = null;
+
+        try{
+            Class.forName("net.sourceforge.jtds.jdbc.Driver");
+            ConnectionURL= "jdbc:jtds:sqlserver://"+ ip + ":"+ port+";"+ "databasename="+ database+";user="+uname+";password="+pass+";";
+            connection = DriverManager.getConnection(ConnectionURL);
+            System.out.println("sucess");
+        }
+        catch(Exception e){
+            Log.e("Error",e.getMessage());
+        }
+        return connection;
+    }
+}
--- Bank/app/src/main/java/com/example/bank/SignIn.kt
+++ Bank/app/src/main/java/com/example/bank/SignIn.kt
@@ -3,11 +3,18 @@ package com.example.bank
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.annotation.RequiresPermission
+import java.sql.Connection
+import java.sql.ResultSet
+import java.sql.Statement
 
 class SignIn : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
@@ -23,9 +30,48 @@ class SignIn : AppCompatActivity() {
                 Toast.makeText(this, "please enter your Password", Toast.LENGTH_SHORT).show()
             }
             else{
-                val intent = Intent(this,AccountScreen1::class.java);
-                startActivity(intent)
+                val name = et_id.text.toString()
+                val pass = et_password.text.toString()
+                if(GetTextSQL(name,pass)) {
+                    val intent = Intent(this, AccountScreen1::class.java);
+                    startActivity(intent)
+                }
+                else{
+                    Toast.makeText(this, "The Username or Password is incorrect", Toast.LENGTH_SHORT).show()
+                }
             }
         }
     }
+
+
+    fun GetTextSQL(id : String, pass : String) : Boolean {
+        try{
+            val connectionhelper : ConnectionHelper = ConnectionHelper()
+            val connect : Connection = connectionhelper.connectionclass(id,pass)
+            if(connect!=null) {
+                val query : String = "Select CID from Customer where CID = $id and Pass = $pass";
+                val st : Statement = connect.createStatement()
+                val rs : ResultSet = st.executeQuery(query)
+                if (!rs.next()) {
+                    println("ResultSet in empty in Java")
+                    return false
+                }
+                else {
+                    do {
+                        val data = rs.getString(1);
+                        return true
+                    } while (rs.next()) }
+
+            }
+            connect.close()
+
+        }
+        catch (e:Exception){
+            Log.e("Errorss", e.message!!)
+        }
+        return false
+    }
+
+
+
 }
